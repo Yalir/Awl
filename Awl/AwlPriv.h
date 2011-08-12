7e3d7a6619c13428582032d08a96e31bf9993845
Ceylo <soltic.lucas@gmail.com>
2011-08-12 19:45:09 +0200

    Initial Commit

---
+++ Awl/AwlPriv.h
@@ -0,0 +1,19 @@
+/*
+ *  AwlPriv.h
+ *  Awl
+ *
+ *  Created by Ceylo on 12/08/11.
+ *  Copyright 2011 Yalir. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class AwlPriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
