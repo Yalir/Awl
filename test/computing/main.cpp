6261e486e8edabb8fae6ef89facda3dbffee438d
Ceylo <soltic.lucas@gmail.com>
2011-09-19 00:40:03 +0200

    Updated sample for new API and added more tests.

--- test/computing/main.cpp
+++ test/computing/main.cpp
@@ -1,52 +1,91 @@
-//
-//  main.cpp
-//  awl_test
-//
-//  Created by Ceylo on 12/08/11.
-//  Copyright 2011 Yalir. All rights reserved.
-//
+
+/*
+ *  computing/main.cpp
+ *  Awl - Asynchronous Work Library
+ *
+ *  Copyright (c) 2011 Lucas Soltic
+ *  ceylow@gmail.com
+ *
+ *  This software is provided 'as-is', without any express or implied warranty.
+ *  In no event will the authors be held liable for any damages arising from
+ *  the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any purpose,
+ *  including commercial applications, and to alter it and redistribute it freely,
+ *  subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented;
+ *     you must not claim that you wrote the original software.
+ *     If you use this software in a product, an acknowledgment
+ *     in the product documentation would be appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such,
+ *     and must not be misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source distribution.
+ *
+ */
 
 #include <Awl/Awl.hpp>
 #include <Awl/Debug.hpp>
 
+int counter = 0;
+
 class TestClass {
 public:
-	void test(void)
+	void method(awl::Task *self)
 	{
-		std::cout << "check point 1 ok" << std::endl;
+		MT_COUT(std::cout << "check point " << __LINE__ << " ok:" << ++counter << std::endl);
 	}
 	
+	static void static_method(awl::Task *self)
+	{
+		MT_COUT(std::cout << "check point " << __LINE__ << " ok:" << ++counter << std::endl);
+		
+		AwlAsyncBlock
+		({
+			MT_COUT(std::cout << "check point " << __LINE__ << " ok:" << ++counter << std::endl);
+		})
+	}
 };
 
-void some_func(void)
+void function(awl::Task *self)
 {
-	DISPLAY_THREAD_ID;
-	std::cout << "function!" << std::endl;
+	MT_COUT(std::cout << "check point " << __LINE__ << " ok:" << ++counter << std::endl);
 	
-	AwlMainThreadBlock
+	self->Abort();
+	
+	// Should never be executed
+	AwlAsyncMainThreadBlock
 	({
-		DISPLAY_THREAD_ID;
-		std::cout << "o_o" << std::endl;
-		awl::WorkLoop::Default().Stop();
+		MT_COUT(std::cout << "check point " << __LINE__ << " ok:" << ++counter << std::endl);
 	})
 }
 
 int main (int argc, const char * argv[])
 {	
-	for (int i = 0; i < 50;i++)
+	for (int i = 0; i < 3;i++)
 	{
 		// Do some parallel computing
 		AwlAsyncBlock
 		({
 			int a = 1;
 			for (int i = 0; i < 1000000;i++)
-				for (int j = 0; j < 100;j++)
+				for (int j = 0; j < 10;j++)
 					a = a * (a + 1);
+			
+			MT_COUT(std::cout << "check point " << __LINE__ << " ok:" << ++counter << std::endl);
 		})
 	}
 	
+	TestClass obj;
+	
+	AwlAsyncCall(function);
+	AwlAsyncCall(TestClass::static_method);
+	AwlAsyncMethod(&TestClass::method, &obj);
 	
 	awl::ThreadPool::WaitAndDie();
+	awl::WorkLoop::Default().Run(); // call here to be sure function() has been executed
 	
     return 0;
 }
